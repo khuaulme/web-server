6f0894ed670e2902b48a68ac5e1308aae9059625
Karen Wilder <karen.huaulme@gmail.com>
2016-05-24 21:27:06 -0700

    refactor middleware

---
+++ middleware.js
@@ -0,0 +1,15 @@
+// use modules export to export middleware
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' +
+			req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+module.exports = middleware;
